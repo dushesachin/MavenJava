207fd13eadbe52e3a18c3a800856d85bdad53d5f
Sachin <dushesachin@gmail.com>
2019-09-25 18:39:02 +1000

    just a space

--- src/test/java/TestingPurpose/MavenJava/DesktopAppTest.java
+++ src/test/java/TestingPurpose/MavenJava/DesktopAppTest.java
@@ -3,7 +3,7 @@ package TestingPurpose.MavenJava;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
-public class DesktopAppTest {
+public class DesktopAppTest  {
 	@Test
 	public void getDesktopAppData() {
 		System.out.println("desktop app data");
