e57acfab8ee81e9437808de6844bda7185d403b4
Sachin <dushesachin@gmail.com>
2019-09-27 13:11:44 +1000

    27th sep commit

--- src/main/java/TestingPurpose/MavenJava/BaseTest.java
+++ src/main/java/TestingPurpose/MavenJava/BaseTest.java
@@ -3,7 +3,8 @@ package TestingPurpose.MavenJava;
 public class BaseTest {
 
 	public static void main(String[] args) {
-		System.out.println("Base Test");
+		System.out.println("Base Test ");
+		System.out.println("adding new steps");
 
 	}
 
